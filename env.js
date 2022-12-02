e28f18a6386339975fd52d178269417aa3f2edec
Daniel Aguado Huaccharaqui <daniel030891@gmail.com>
2022-12-01 23:37:53 -0500

    comment api version

--- env.js
+++ env.js
@@ -104,7 +104,7 @@ var
 
   ////////uncomment the following line when downloading the app
 
-  apiUrl = 'https://js.arcgis.com/3.38';
+  // apiUrl = 'https://js.arcgis.com/3.38';
 
   //////////////////////////////////////////////////////////////
   allCookies = getAllCookies();
